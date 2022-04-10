fb90a820283cafe50292c52e9f252f5f0cf7498c
guddu <kguddu.1011@gmail.com>
2022-04-10 12:13:56 +0530

    file added

---
+++ input.js
@@ -0,0 +1,13 @@
+//var input=document.querySelector('input');
+var button=document.querySelector('button');
+//var alert=document.createElement('alert')
+
+button.addEventListener('click',alert1);
+
+function alert1(){
+    var inputVal=document.getElementById("myinput").value;
+    alert(inputVal);
+    
+
+};
+
